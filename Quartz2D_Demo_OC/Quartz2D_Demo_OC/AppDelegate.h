c1f6103a35b29f2ac5d62f15373377df45ce416d
aTreey <480814177@qq.com>
2016-12-04 13:30:49 +0800

    新建工程

---
+++ Quartz2D_Demo_OC/Quartz2D_Demo_OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quartz2D_Demo_OC
+//
+//  Created by Hongpeng Yu on 2016/12/4.
+//  Copyright © 2016年 Hongpeng Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quartz2D_Demo_OC/Quartz2D_Demo_OC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quartz2D_Demo_OC
+//
+//  Created by Hongpeng Yu on 2016/12/4.
+//  Copyright © 2016年 Hongpeng Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
